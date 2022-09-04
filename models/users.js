3f59ca1ef7cf652c3224e6d860836c1afe4eb149
prieto <jersonamendez@gmail.com>
2022-09-04 13:41:53 -0400

    openeyes baby

---
+++ models/users.js
@@ -0,0 +1,22 @@
+//user model for http api
+let users = [{fisrtName:'William',middleName: '',lastName: 'Gomez',
+email:'williamgomez@gmail.com',maritalStatus: 'Single',
+phone:'5618531537',gender:'Male',
+address1:'2000 N Dixie Hwy',address2:'',city:'Lake Worth',
+state:'FL',zipcode:'33460',
+driversLicense:'1234567834219',passport:null,
+ssn:'12345789',dob:'07/23/1987',
+vin:'JTMZD33V266020244',
+mileage:'1234',
+pipDeductible:'1000',
+collisionDeductible:'500',
+comprehensiveDeductible:'500',
+rentalDeductible:'75',
+towingDeductible:'75',
+propertyDamageCoverage:'10',
+medicalCoverage:null,
+biCoverage:'10/20',
+uninsuredMotorist:null,homeOwner:false,homeOwnership:"Rent"
+}];
+
+export default users;
